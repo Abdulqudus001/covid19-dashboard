2dc15514bc65caea4c91cef52f8693c32e70f88f
Abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-03-22 18:31:46 +0100

    Setup vuex

---
+++ src/components/statsCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="stats" :style="{background: $vuetify.theme.themes[theme].header}">
+    <v-icon :class="type">{{ icon }}</v-icon>
+    <p>{{ count }}</p>
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    count: {
+      type: Number,
+      default: 0,
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'danger',
+    },
+  },
+  computed: {
+    theme() {
+      return (this.$vuetify.theme.dark) ? 'dark' : 'light';
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .stats {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    padding: 20px;
+    border-radius: 12px;
+    p {
+      margin: 10px 0;
+    }
+
+    i {
+      width: 50px;
+      margin: 0 auto;
+      height: 50px;
+      font-size: 1.8rem;
+      border-radius: 50%;
+      text-align: center;
+
+      &.infected {
+        color: #00CFE8;
+        background-color: rgba(0, 207, 232, 0.2);
+      }
+
+      &.deaths {
+        color: #cf270a;
+        background-color: rgba(207, 40, 10, 0.2);
+      }
+
+      &.recovered {
+        color: #28C76F;
+        background-color: rgba(40, 199, 111, 0.2);
+      }
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,46 @@
 <template>
   <v-container>
-    Hello from this side
+    <v-layout justify-center row wrap>
+      <v-flex sm12 md2 class="mx-2 my-2">
+        <stats-card
+          icon="mdi-alert-outline"
+          :count="worldStats.cases"
+          name="Total cases"
+          type="infected"
+        />
+      </v-flex>
+      <v-flex sm12 md2 class="mx-2 my-2">
+        <stats-card
+          icon="mdi-alert-octagon-outline"
+          :count="worldStats.cases"
+          name="Total cases"
+          type="deaths"
+        />
+      </v-flex>
+      <v-flex sm12 md2 class="mx-2 my-2">
+        <stats-card
+          icon="mdi-thumb-up-outline"
+          :count="worldStats.cases"
+          name="Total cases"
+          type="recovered"
+        />
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {
+import StatsCard from '@/components/statsCard.vue';
 
+export default {
+  data: () => ({
+    worldStats: {
+      cases: 319010,
+      deaths: 13685,
+      recovered: 96006,
+    },
+  }),
+  components: { StatsCard },
 };
 </script>
 
