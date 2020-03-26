b73e67e18b35eb728226f6c9afc448b6dc37f867
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-03-26 13:36:02 +0100

    Saved store data in local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersist from 'vuex-persist';
 
 import state from './state';
 import actions from './actions';
@@ -8,11 +9,17 @@ import getters from './getters';
 
 Vue.use(Vuex);
 
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex',
+  storage: window.localStorage,
+});
+
 export default new Vuex.Store({
   state,
   mutations,
   actions,
   getters,
+  plugins: [vuexLocalStorage.plugin],
   modules: {
   },
 });
