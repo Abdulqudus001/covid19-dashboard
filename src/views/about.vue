0aab65084f15c99dde48e68cf111a8fa88af2b42
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-03-27 12:17:38 +0100

    Added links to about page

--- src/views/about.vue
+++ src/views/about.vue
@@ -3,6 +3,23 @@
     <v-layout justify-center align-center class="display-2 font-weight-bold text-center">
       Coming soon
     </v-layout>
+    <v-layout justify-center align-center>
+      <a
+        href="#/"
+        class="link"
+        :class="$vuetify.theme.dark ? 'link-dark' : 'link-light'"
+      >Home</a>
+      <a
+        href="#/news"
+        class="link"
+        :class="$vuetify.theme.dark ? 'link-dark' : 'link-light'"
+      >News</a>
+      <a
+        href="#/countries"
+        class="link"
+        :class="$vuetify.theme.dark ? 'link-dark' : 'link-light'"
+      >Countries</a>
+    </v-layout>
   </v-container>
 </template>
 
