dc5307321a0d382261f7e32e3319832af41c67cf
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-04-04 17:17:48 +0100

    Reduced transition speed

--- src/App.vue
+++ src/App.vue
@@ -85,10 +85,10 @@ export default {
   }
 
   .slide-fade-enter-active {
-    transition: all .3s ease;
+    transition: all .1s ease;
   }
   .slide-fade-leave-active {
-    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
   }
   .slide-fade-enter, .slide-fade-leave-to {
     transform: translateX(10px);
